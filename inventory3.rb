bcd8aeb66a9c22d6f637ba839870efd6ee86826f
Erin Hinson <hinson.erin@gmail.com>
2014-11-14 18:18:52 -0500

    User can continue to update inventory until they "quit"

--- inventory3.rb
+++ inventory3.rb
@@ -1,3 +1,4 @@
+
 # output the current items in a number list and their current counts to the screen
 
 array = [["item1", "count1"],
@@ -13,24 +14,42 @@ def print_inventory(array)
 		puts "#{j+ 1}. #{i} ".center(lineWidth/2)+ "#{count}".center(lineWidth/2)
 		j+=1
 	end
+	puts "put \"quit \" to quit"
+
 end
 
 print_inventory(array)
 
+quit = false
+
 # prompt the user to enter which item should be edited.
-puts "Please select the number of the item to update:"
-user_choice = gets.chomp.to_i
+#keep going until user chooses to quit
+until quit == true
+	puts "Please select the number of the item to update:"
+	user_choice = gets.chomp.to_i
 
-# After the user chooses the item to edit, the users should be allowed to enter a new inventory count for the item
-if user_choice > 0 && user_choice < 5
-	index = user_choice - 1
-	puts "Please enter the new count for this item: "
-	array[index][1]= gets.chomp
+	# After the user chooses the item to edit, the users should be allowed to enter a new inventory count for the item
+	if user_choice == "quit".downcase || user_choice == 0
+		quit = true
+		puts "Program Terminated"
+		break
 
-	print_inventory(array)
-else
-	puts "Please enter a choice between 1 and #{array.length}"
+	elsif user_choice > 0 && user_choice < array.length
+		index = user_choice - 1
+		puts "Please enter the new count for this item: "
+		new_count = gets.chomp.downcase
+		if new_count == "quit"
+			quit = true
+			puts "Program Terminated"
+			break
+		else
+			array[index][1]= new_count
+			print_inventory(array)
+		end
+
+	else
+		puts "Please enter a choice between 1 and #{array.length}"
+	end
 end
-
 
 
